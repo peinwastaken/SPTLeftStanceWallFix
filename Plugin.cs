5b0b76918140af4ebcd3dc56314936e2c15935c9
peinwastaken <legokid173@gmail.com>
2025-01-26 18:35:34 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using EFT;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace SPTLeftStanceWallFix
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new DisableLeftStanceFromHandsPatch().Enable();
+        }
+    }
+
+    public class DisableLeftStanceFromHandsPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(LeftStanceController).GetMethod(nameof(LeftStanceController.DisableLeftStanceAnimFromHandsAction));
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix(LeftStanceController __instance)
+        {
+            return false; // bye bye!
+        }
+    }
+}
